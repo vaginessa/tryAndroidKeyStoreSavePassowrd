c3c7137d49c6b6f1436545c993f2738cfab9e558
roy989898 <roy989898@gmail.com>
2017-08-03 11:18:02 +0800

    can show the saved key store

---
+++ app/src/main/java/com/roy/tryandroidkeystore/Constants.java
@@ -0,0 +1,9 @@
+package com.roy.tryandroidkeystore;
+
+/**
+ * Created by roy.leung on 3/8/2017.
+ */
+
+public class Constants {
+    static String KEY_STORE_NAME = "AndroidKeyStore";
+}
--- app/src/main/java/com/roy/tryandroidkeystore/MainActivity.java
+++ app/src/main/java/com/roy/tryandroidkeystore/MainActivity.java
@@ -7,12 +7,18 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.util.ArrayList;
+import java.util.Enumeration;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private EditText etKeyInput;
     private Button btSave;
     private TextView tvShowKey;
     private Button btShow;
+    private KeyStore keyStore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +33,30 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btSave.setOnClickListener(this);
         btShow.setOnClickListener(this);
 
+        try {
+            keyStore = KeyStore.getInstance(Constants.KEY_STORE_NAME);
+            keyStore.load(null);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    private ArrayList<String> getTheStoreKeys(KeyStore keyStore) {
+        ArrayList<String> keyLists = new ArrayList<>();
+        try {
+            Enumeration<String> aliases = keyStore.aliases();
+            while (aliases.hasMoreElements()) {
+                keyLists.add(aliases.nextElement());
+            }
+
+        } catch (KeyStoreException e) {
+            e.printStackTrace();
+        } finally {
+            return keyLists;
+        }
+
 
     }
 
@@ -37,6 +67,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
 
             case R.id.bt_show:
+                if (keyStore == null)
+                    return;
+
+                ArrayList<String> keyList = getTheStoreKeys(keyStore);
+                String tempString = "";
+                for (String key : keyList) {
+                    tempString = tempString + key + "\n";
+                }
+
+                if (keyList.isEmpty()) {
+                    tvShowKey.setText("empty");
+                } else {
+                    tvShowKey.setText(tempString);
+                }
+
+
                 break;
         }
 
