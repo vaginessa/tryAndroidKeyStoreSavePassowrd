d3dca84488bc3f3bbf321028e33eb3f880c01cde
roy989898 <roy989898@gmail.com>
2017-08-03 10:49:03 +0800

    add onClick Listener

--- app/src/main/java/com/roy/tryandroidkeystore/MainActivity.java
+++ app/src/main/java/com/roy/tryandroidkeystore/MainActivity.java
@@ -1,13 +1,46 @@
 package com.roy.tryandroidkeystore;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText etKeyInput;
+    private Button btSave;
+    private TextView tvShowKey;
+    private Button btShow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etKeyInput = (EditText) findViewById(R.id.et_key_input);
+        btSave = (Button) findViewById(R.id.bt_save);
+        tvShowKey = (TextView) findViewById(R.id.tv_show_key);
+        btShow = (Button) findViewById(R.id.bt_show);
+
+        btSave.setOnClickListener(this);
+        btShow.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.bt_save:
+                break;
+
+            case R.id.bt_show:
+                break;
+        }
+
     }
 }
+
+
